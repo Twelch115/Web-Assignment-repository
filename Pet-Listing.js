7ed18248fa9e47ec7745492ccb1314a0b801b3b9
Thomas Welch <thomaswelch115@gmail.com>
2021-10-08 11:05:14 +0100

    pet JS page and fixes

---
+++ Pet-Listing.js
@@ -0,0 +1,45 @@
+// list of products with names, descriptions and prices.
+var Pets = {
+    101: {
+        Name : "Larry",
+        Breed : "Parot",
+        Desc : "3 years-old, green feathers, bubbly personality",
+        Img : "",
+        Price : 25,
+    },
+    102: {
+        Name : "Felix",
+        Breed : "Finch",
+        Desc : "6 years-old, grey/orange feathers, quiet personality",
+        Img : "",
+        Price : 40,
+    },
+    103: {
+        Name : "Sarah",
+        Breed : "Rabbit",
+        Desc : "2 years-old, white fur, curious personality",
+        Img : "",
+        Price : 15,
+    },
+    104: {
+        Name : "Ryan",
+        Breed : "Gerbil",
+        Desc : "4 years-old , brown fur, excited personality",
+        Img : "",
+        Price : 20,
+    },
+    105: {
+        Name : "Ryan",
+        Breed : "Salamander",
+        Desc : "4 years-old , green scales, relaxed personality",
+        Img : "",
+        Price : 30,
+    },
+    106: {
+        Name : "Oswald",
+        Breed : "Komodo Dragon",
+        Desc : "8 year-old, brown//grey scales, sleepy personality",
+        Img : "",
+        Price : 60,
+    },
+}
