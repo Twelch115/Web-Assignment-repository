b064ff32f466b44ba223d91e35d09d31fe8ab06e
Thomas Welch <thomaswelch115@gmail.com>
2021-10-14 09:21:19 +0100

    new pets and fixes

--- JS-Cart/Accesories-Listing.js
+++ JS-Cart/Accesories-Listing.js
@@ -1,9 +1,9 @@
 //Lists out all available pet accessories with, names, descriptions, images and prices.
 var accessories = {
     301: {
-        Name : "Larry", //item's name
-        Desc : "3 years-old, blue/green/yellow feathers, bubbly personality", //description of the item
+        Name : "Food Bowl", //item's name
+        Desc : "standard pet food bowl, can be filed with many kinds of foods. comes in blue, black and grey", //description of the item
         Img : "../images/Pet Images/Larry.jpg", //img file link
-        Price : 25, //item's price
+        Price : 12, //item's price
     },
 }
